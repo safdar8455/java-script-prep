5a0c55adbf9f2654feff266d945709e549a678c2
Safdar Hussain <112075027+safdar8455@users.noreply.github.com>
2025-01-10 08:31:37 +0000

    uper triangle of holo diamond completed

--- First/code1.js
+++ First/code1.js
@@ -2,8 +2,17 @@ let n = 4;
 // upper
 for (let i = 0; i < n; i++) {
     let row = "";
-    for (let j = 0; j <= i; j++) {
+    for (let j = n-1; j > i; j--) {
+        row += " ";
+    }
+    row += "*";
+    if(i!=0){
+        for (let j = 0; j <2* i-1; j++) {
+                    
+            row += " ";
+        }
         row += "*";
+
     }
     console.log(row); // Print the row after constructing it
 }
